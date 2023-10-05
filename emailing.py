caa885b77bd60ceb04c78800677124045c79a62c
aasif <asifali8121@gmail.com>
2023-10-05 10:25:30 +0500

    added email functionality and remove images from folder

--- emailing.py
+++ emailing.py
@@ -1,3 +1,26 @@
-def send_email():
+import smtplib
+import imghdr
+from email.message import EmailMessage
 
-    print("Email was sent")
+PASSWORD = "..."
+SENDER = "app8flask@gmail.com"
+RECEIVER = "app8flask@gmail.com"
+def send_email(image_path):
+
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer!")
+
+    # rb = reading binary mode
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, mainType="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+
+if __name__ == "__main__":
+    send_email(image_path="images/19.png")
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import time
 from emailing import send_email
 import glob
+import os
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -10,6 +11,12 @@ first_frame = None
 status_list = []
 
 count = 1
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 
 while True:
     status = 0
@@ -64,7 +71,8 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
+        clean_folder()
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
